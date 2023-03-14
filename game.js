d14eb0dc33a5f108082c9a32d9c38b274afbaa82
Leonardo <62109173+leogbittante@users.noreply.github.com>
2023-03-14 14:39:28 -0300

    First Upload Simon Game

---
+++ game.js
@@ -0,0 +1,18 @@
+var gamePattern = []
+
+var buttonColours = ["red", "blue", "green", "yellow"]
+
+var randomChosenColour = buttonColours[nextSequence()]
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4)
+    return randomNumber
+}
+
+gamePattern.push(randomChosenColour)
+
+
+$("button").
+
+
+
